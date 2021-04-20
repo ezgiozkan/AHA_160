609326e5fab0398b81503cbac561656758415404
batuhanbaran <fb_batuhan_baran@hotmail.com>
2021-04-21 00:03:00 +0300

    VaccineVC is added

---
+++ HSU/ReminderViewController/VaccineViewController/VaccineViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VaccineViewController.swift
+//  HSU
+//
+//  Created by Batuhan Baran on 21.04.2021.
+//
+
+import UIKit
+
+class VaccineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
