55d5650cd545cebd50c83ac9b38f08d7457ce690
batuhanbaran <fb_batuhan_baran@hotmail.com>
2021-04-25 18:32:05 +0300

    settingsVC is created

---
+++ HSU/SettingsViewController/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  HSU
+//
+//  Created by Batuhan Baran on 25.04.2021.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
